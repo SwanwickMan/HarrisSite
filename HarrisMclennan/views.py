dd28dbad3612af6a5e567329adefd478a8692e60
Andrew Laing <2775878l@student.gla.ac.uk>
2024-09-21 02:49:42 +0100

    Stuff?

---
+++ HarrisMclennan/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context_dict = {}
+    response = render(request, 'HarrisMclennan/index.html', context=context_dict)
+    return response
